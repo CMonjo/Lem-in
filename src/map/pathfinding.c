43b6cc4c534399c02362c3a53845afcb0582456e
Slaymd <darius.martin@epitech.eu>
2018-03-23 16:02:08 +0100

    (Pathfinding) added prototype

---
+++ src/map/pathfinding.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2018
+** CPE_lemin_2017
+** File description:
+** path finding
+*/
+
+#include "../../include/lemin.h"
+#include "../../include/my.h"
+
+path_t	*get_path_between(room_t *src, room_t *dest)
+{
+	return (NULL);
+}
